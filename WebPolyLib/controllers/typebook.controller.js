f37b2ec1ed200999f28245d690e6f6e6eb3d4ce4
KienPC-KEN <phungchikien196@gmail.com>
2023-07-21 00:42:56 +0700

    .

--- WebPolyLib/controllers/typebook.controller.js
+++ WebPolyLib/controllers/typebook.controller.js
@@ -1,10 +1,31 @@
+const mongoose = require('mongoose');
+const typeBookModel = require('../model/typeBookModel');
+const uri = 'mongodb+srv://phungchikien196:Qa4168ciXnRnjV9G@apppolylib.5gjczzc.mongodb.net/PolyLib?retryWrites=true&w=majority';
+mongoose.connect(uri);
+console.log("Kết nối DB thành công");
+
 
 exports.listType = (req, res) => {
+
     res.render('typebook/listtypebook', {
         layout: 'main'
     })
 }
-exports.addType = (req, res) => {
+
+exports.addType = async (req, res) => {
+    let arrTypeBook = await typeBookModel.find().lean();
+
+    const { nametypebook } = req.body;
+    var newStt = arrTypeBook[arrTypeBook.length - 1].stt + 1;
+    let objTypeBook = { stt: newStt, nametypebook: nametypebook };
+    if (!nametypebook) {
+        console.log("lỗi");
+    } else {
+        let kq = await typeBookModel.insertMany(objTypeBook);
+        res.redirect('/typeBook');
+        console.log(kq);
+
+    }
     res.render('typebook/addtype', {
         layout: 'main'
     })
