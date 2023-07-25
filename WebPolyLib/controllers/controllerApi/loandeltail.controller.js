7d9510d8c452a1daada7b6bbc2cf8235802c7919
Nguyễn Chính <you@example.com>
2023-07-25 16:41:04 +0700

    up api loan deltail

--- WebPolyLib/controllers/controllerApi/loandeltail.controller.js
+++ WebPolyLib/controllers/controllerApi/loandeltail.controller.js
@@ -40,40 +40,117 @@ exports.listLoanDeltail = async (req, res, next) => {
     }
 };
 
+//add loand deltail
+exports.addLoand = async (req, res, next) => {
+    try {
+        const { userName, bookTitle, librarianName, startDate, endDate, phoneUser, price, status } = req.body;
 
-//list book
-exports.listTypeBook = async (req, res, next) => {
-    await mongoose.connect(uri);
-    let ds = await mdbook.find();
-    console.log(ds + " list book");
-    if (ds) {
-        return res.status(200).json({ data: ds, check: 'có dữ liệu' });
+        // Tìm id của người mượn dựa vào tên thành viên
+        const user = await mduser.findOne({ name: userName });
+        if (!user) {
+            return res.status(404).json({ error: 'Không tìm thấy thông tin người mượn.' });
+        }
+
+        // Tìm id của sách dựa vào tên sách
+        const book = await mdbook.findOne({ name: bookTitle });
+        if (!book) {
+            return res.status(404).json({ error: 'Không tìm thấy thông tin sách.' });
+        }
+
+        // Tìm id của thành viên dựa vào tên thủ thư và số điện thoại
+        const member = await mdmember.findOne({ name: librarianName, phone: phoneUser });
+        if (!member) {
+            return res.status(404).json({ error: 'Không tìm thấy thông tin thành viên.' });
+        }
+
+        // Kiểm tra xem giá, ngày bắt đầu và ngày kết thúc có hợp lệ hay không
+        if (!price || !startDate || !endDate) {
+            console.log(startDate, endDate);
+            return res.status(400).json({ error: 'Thiếu thông tin giá, ngày bắt đầu hoặc ngày kết thúc.' });
+        }
+
+        // Tạo phiếu mượn mới
+        const newLoan = new mdloandeltail({
+            _idUser: user._id,
+            _idBook: book._id,
+            _idMember: member._id,
+            price,
+            startDate,
+            endDate,
+            status: status || 'no', // Sử dụng trạng thái "no" nếu không có trạng thái được cung cấp
+        });
+
+        // Lưu phiếu mượn vào cơ sở dữ liệu
+        const savedLoan = await newLoan.save();
+
+        return res.status(201).json({ data: savedLoan, message: 'Thêm phiếu mượn thành công.' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-    else {
-        return res.status(404).json({ check: 'không có dữ liệu' });
+};
+
+//update
+exports.updateLoan = async (req, res, next) => {
+    try {
+        const { userName, bookTitle, librarianName, startDate, endDate, phoneUser, price, status , _id } = req.body;
+
+        // Tìm id của người mượn dựa vào tên thành viên
+        const user = await mduser.findOne({ name: userName });
+        if (!user) {
+            return res.status(404).json({ error: 'Không tìm thấy thông tin người mượn.' });
+        }
+
+        // Tìm id của sách dựa vào tên sách
+        const book = await mdbook.findOne({ name: bookTitle });
+        if (!book) {
+            return res.status(404).json({ error: 'Không tìm thấy thông tin sách.' });
+        }
+
+        // Tìm id của thành viên dựa vào tên thủ thư và số điện thoại
+        const member = await mdmember.findOne({ name: librarianName, phone: phoneUser });
+        if (!member) {
+            return res.status(404).json({ error: 'Không tìm thấy thông tin thành viên.' });
+        }
+
+        // Kiểm tra xem giá, ngày bắt đầu và ngày kết thúc có hợp lệ hay không
+        if (!price || !startDate || !endDate) {
+            console.log(startDate, endDate);
+            return res.status(400).json({ error: 'Thiếu thông tin giá, ngày bắt đầu hoặc ngày kết thúc.' });
+        }
+
+        // Tìm phiếu mượn cần cập nhật dựa vào loanId
+        const loanToUpdate = await mdloandeltail.findById(_id);
+        if (!loanToUpdate) {
+            return res.status(404).json({ error: 'Không tìm thấy thông tin phiếu mượn cần cập nhật.' });
+        }
+        // Cập nhật thông tin phiếu mượn
+        loanToUpdate._idUser = user._id;
+        loanToUpdate._idBook = book._id;
+        loanToUpdate._idMember = member._id;
+        loanToUpdate.price = price;
+        loanToUpdate.startDate = startDate;
+        loanToUpdate.endDate = endDate;
+        loanToUpdate.status = status || 'no'; // Sử dụng trạng thái "no" nếu không có trạng thái được cung cấp
+
+        // Lưu phiếu mượn đã cập nhật vào cơ sở dữ liệu
+        const updatedLoan = await loanToUpdate.save();
+
+        return res.status(201).json({ data: updatedLoan, message: 'Sửa phiếu mượn thành công.' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-}
-//list member
-exports.listTypeBook = async (req, res, next) => {
-    await mongoose.connect(uri);
-    let ds = await mdmember.find();
-    console.log(ds + " list book");
-    if (ds) {
-        return res.status(200).json({ data: ds, check: 'có dữ liệu' });
+};
+
+//delete 
+exports.deleteLoan = async (req, res, next) => {
+    const { _id } = req.body;
+    try {
+        const deleted = await mdloandeltail.findByIdAndDelete(_id);
+        if (!deleted) {
+            return res.json({ status: 0, message: 'Không tìm thấy dữ liệu' });
+        }
+        res.json({ status: 1, message: 'Xóa thành công', data: deleted });
+    } catch (err) {
+        res.json({ status: 0, message: 'Xóa thất bại', error: err.message });
     }
-    else {
-        return res.status(404).json({ check: 'không có dữ liệu' });
-    }
-}
-//list user
-exports.listTypeBook = async (req, res, next) => {
-    await mongoose.connect(uri);
-    let ds = await mduser.find();
-    console.log(ds + " list book");
-    if (ds) {
-        return res.status(200).json({ data: ds, check: 'có dữ liệu' });
-    }
-    else {
-        return res.status(404).json({ check: 'không có dữ liệu' });
-    }
-}
+};
--- WebPolyLib/router/routerAPI/loandeltial.router.js
+++ WebPolyLib/router/routerAPI/loandeltial.router.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const api_loandeltail = require('../../controllers/controllerApi/loandeltail.controller');
+
 router.get('/loandeltail', api_loandeltail.listLoanDeltail);
+router.post('/addloan',api_loandeltail.addLoand);
+router.put('/updateloan',api_loandeltail.updateLoan);
+router.delete('/deleteloan',api_loandeltail.deleteLoan);
 
 module.exports = router;
