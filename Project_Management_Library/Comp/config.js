a3dcf22483ebb95681568f70c0fb75b11e74ae2e
Nguyễn Chính <you@example.com>
2023-08-04 15:29:50 +0700

    up

--- Project_Management_Library/Comp/config.js
+++ Project_Management_Library/Comp/config.js
@@ -1,4 +1,4 @@
 // config.js hoặc constants.js
-export const API_IP = '10.24.44.132'; // Thay đổi địa chỉ IP thành địa chỉ IP thật của máy chủ của bạn
+export const API_IP = '192.168.221.42'; // Thay đổi địa chỉ IP thành địa chỉ IP thật của máy chủ của bạn
 //Ip của Kiên ở nhà: 192.168.1.4
 //IP của Chính ở nhà: 192.168.1.160
--- WebPolyLib/router/routerAPI/login.router.js
+++ WebPolyLib/router/routerAPI/login.router.js
@@ -6,4 +6,5 @@ router.get('/getdata', logn_controller.authenticateToken, (req, res) => {
     res.json({ message: 'Bạn đã truy cập vào route được bảo vệ bằng token.' });
 });
 router.post('/login', logn_controller.login);
+router.post('/accurary', logn_controller.getUserInfo);
 module.exports = router;
